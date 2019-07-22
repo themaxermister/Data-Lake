33c47b04337426fc40af44408faa010ec138459f
themaxermister <subscribingalerts@gmail.com>
2019-07-23 01:51:56 +0800

    Update

--- etl.py
+++ etl.py
@@ -121,7 +121,7 @@ def process_log_data(spark, input_data, output_data):
     ])
     
     # get filepath to log data file
-    log_data = input_data + "log_data/*.json"       #"log_data/*/*/*.json"
+    log_data = input_data + "log_data/*/*/*.json"
 
     # read log data fil
     df = spark.read.json(log_data, schema=log_data_schema)
